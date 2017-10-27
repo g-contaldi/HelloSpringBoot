ca84769da85dc06c549e76199f9df29e0a4fa5aa
gianluca <g.contaldi@dstech.it>
2017-10-27 11:12:07 +0200

    added user auth controller

---
+++ src/main/java/it/dstech/controller/AuthController.java
@@ -0,0 +1,43 @@
+package it.dstech.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.dstech.model.User;
+import it.dstech.services.UserService;
+import it.dstech.services.auth.AuthService;
+
+@RestController
+public class AuthController {
+
+	@Autowired
+	private AuthService authService;
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private PasswordEncoder encoder;
+
+	@PostMapping("/login")
+	public UserDetails authenticate(@RequestBody User principal) throws Exception {
+		return authService.authenticate(principal);
+	}
+
+	@PostMapping("/register")
+	public User addUser(@RequestBody User user) {
+		user.setPassword(encoder.encode(user.getPassword()));
+		return userService.saveUser(user);
+	}
+
+	@GetMapping("/getUserModel")
+	public User getModel() {
+		return new User();
+	}
+
+}
